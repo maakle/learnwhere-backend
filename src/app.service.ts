b6794d1f6c44ff30754ba98828b537a41ca73cfa
Mathias Klenk <mathiasklenk@gmx.de>
2021-04-26 19:52:24 -0400

    Added basic html templating with hbs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+type HelloWorldType = {
+  message: string;
+};
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): HelloWorldType {
+    return { message: 'Hello world!' };
   }
 }
