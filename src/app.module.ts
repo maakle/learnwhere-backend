cf025fc373457758c4eb31963a3bd3499f9c5366
Mathias Klenk <mathiasklenk@gmx.de>
2021-04-26 22:34:38 -0400

    removed verbose code around index

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { IssuerModule } from './issuer/issuer.module';
 import { VerifierModule } from './verifier/verifier.module';
 
 @Module({
   imports: [IssuerModule, VerifierModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
