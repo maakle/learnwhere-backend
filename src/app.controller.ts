106e29b0f50e7d46a46322669268376d07c30c64
Mathias Klenk <mathiasklenk@gmx.de>
2021-05-07 23:46:12 -0400

    Easter egg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
+import { Get } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get('/')
+  async hello() {
+    return 'Never stop learning. ;) ';
+  }
+}
